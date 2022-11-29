c9a3dca9940c94343d5c4c91e375dc3e5b9959bb
Houser97 <arturo.riverar97@gmail.com>
2022-11-28 21:28:56 -0600

    Remove styled

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -9,7 +9,6 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick';
 import SliderCreated from './slider';
-import styled from "styled-components";
 
 const Carousel = () => {
 
