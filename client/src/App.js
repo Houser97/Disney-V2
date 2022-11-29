f700c00bc9b291732e64685961e869bb5ac1f7bd
Houser97 <arturo.riverar97@gmail.com>
2022-11-28 21:06:20 -0600

    Set up BrowserRouter and comment out unnecesary components

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <BrowserRouter basename='/'>
+    {/*<userContext.Provider value={valueProvider}>*/}
+      <div className="App">
+        <div className='full-height'>
+          <Header/>
+          {/*<Routes >
+            <Route path='/' element = {<Home />}/>
+            <Route path='/search' element = {<Search />}/>
+            <Route path='/watchlist' element = {<Watchlist userID={userID} />} ></Route>
+            <Route path='/originals' element = {<Originals key={"originals-component"} headerRefPlaceholder = {header}/>}/>
+            <Route path='/movies' element = {<MoviesAndSeries key={"movieSectionRender"} moviesSeries={moviesFiltered} titleSection = {"Movies"}  headerRefPlaceholder = {header} />} ></Route>
+            <Route path='/series' element = {<MoviesAndSeries key={"serieSectionRender"} moviesSeries={seriesFiltered} titleSection = {"Series"} headerRefPlaceholder = {header} />} ></Route>
+            <Route path='/login' element = {<LogInSection formToOpen={"1"} key={"logInSection"} headerRef={header} footerRef = {footer} setUserID ={setUserID} userID = {userID} setUsername1 ={setUsernameHeader} />} ></Route>
+            <Route path='/signup' element = {<LogInSection formToOpen={"2"} key={"signUpSection"} headerRef={header} footerRef = {footer} setUserID ={setUserID} userID = {userID} setUsername1 ={setUsernameHeader} />} ></Route>
+            <Route path="/avatar" element = {<ChooseAvatar headerRef={header} footerRef ={footer} setUserPicture = {setUserPictureHeader} />} ></Route>
+          </Routes>
+          <Footer footerRef = {footer}/>*/}
+        </div>
+      </div>
+    {/*</userContext.Provider>*/}
+  </BrowserRouter>
   );
 }
 
