8400910b4cac8e5fc537877ccdaa40dd826b8a3f
Houser97 <arturo.riverar97@gmail.com>
2022-12-01 20:49:13 -0600

    Import images and map array to render them

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -1,23 +1,11 @@
 import '../styles/Carousel.css';
-import Obiwan from '../images/Obi-wan-poster.webp';
-import Daredevil from '../images/Daredevil.png';
-import DrStrange from '../images/DrStrange.png';
-import Rons from '../images/Rons.png';
-import Malcolm from '../images/Malcolm.png';
-import Thor from '../images/Thor.PNG'
+import { carouselImages } from '../assets/constants';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick';
 import SliderCreated from './slider';
 
 const Carousel = () => {
-
-    const movies = [{image: Obiwan},
-                    {image: Daredevil},
-                    {image: DrStrange},
-                    {image: Rons},
-                    {image: Malcolm},
-                    {image: Thor}];
 
     let settings = {
         dots: true,
@@ -31,7 +19,7 @@ const Carousel = () => {
     return(
         <div className='carousel-section' id='carousel'>
             <CarouselSlick {...settings} className = "carousel">
-                {movies.map(
+                {carouselImages.map(
                     function iterateMovies(item, iterator){
                         return(
                             <SliderCreated key={iterator} image={item.image}/>
