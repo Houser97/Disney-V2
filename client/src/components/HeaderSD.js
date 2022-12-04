4d30046bbe971c79cc8c38e042cecc9ddf7b9263
Houser97 <arturo.riverar97@gmail.com>
2022-12-04 14:04:13 -0600

    Set up component

---
+++ client/src/components/HeaderSD.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import '../styles/HeaderSD.css'
+
+const HeaderSD = () => {
+  return (
+    <div className='header-sd-container'>
+        <Link className='link' to = "/">
+            <div className='home option'>
+                <div className='svg-option'>
+                    <svg className='svg' viewBox="0 0 24 24">
+                        <path fill="currentColor" d="M10,20V14H14V20H19V12H22L12,3L2,12H5V20H10Z" />
+                    </svg>
+                </div>
+            </div>
+        </Link>
+        <Link className='link' to = "/search">
+            <div className='search option'>
+                <div className='svg-option'>
+                    <svg className='svg' viewBox="0 0 24 24">
+                        <path fill="currentColor" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
+                    </svg>
+                </div>
+            </div>
+        </Link>
+        <Link className='link' to = "/watchlist" >
+            <div className='watchlist option'>
+                <div className='svg-option plus'>
+                    <svg viewBox="0 0 24 24">
+                        <path fill="currentColor" d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
+                    </svg>
+                </div>
+            </div>
+        </Link>
+    </div>
+  )
+}
+
+export default HeaderSD
