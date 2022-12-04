c7538d439af5f1dfd0a61668f944f56b298d76e0
Houser97 <arturo.riverar97@gmail.com>
2022-12-04 17:33:32 -0600

    Import Routes, Route and Home component to handle routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HeaderSD from './components/HeaderSD';
+import Home from './components/Home';
 
 function App() {
   return (
@@ -12,19 +13,19 @@ function App() {
         <div className='full-height'>
           <Header userID = {false}/>
           <HeaderSD />
-          <Footer />
-          {/*<Routes >
+          <Routes >
             <Route path='/' element = {<Home />}/>
-            <Route path='/search' element = {<Search />}/>
+            {/*<Route path='/search' element = {<Search />}/>
             <Route path='/watchlist' element = {<Watchlist userID={userID} />} ></Route>
             <Route path='/originals' element = {<Originals key={"originals-component"} headerRefPlaceholder = {header}/>}/>
             <Route path='/movies' element = {<MoviesAndSeries key={"movieSectionRender"} moviesSeries={moviesFiltered} titleSection = {"Movies"}  headerRefPlaceholder = {header} />} ></Route>
             <Route path='/series' element = {<MoviesAndSeries key={"serieSectionRender"} moviesSeries={seriesFiltered} titleSection = {"Series"} headerRefPlaceholder = {header} />} ></Route>
             <Route path='/login' element = {<LogInSection formToOpen={"1"} key={"logInSection"} headerRef={header} footerRef = {footer} setUserID ={setUserID} userID = {userID} setUsername1 ={setUsernameHeader} />} ></Route>
             <Route path='/signup' element = {<LogInSection formToOpen={"2"} key={"signUpSection"} headerRef={header} footerRef = {footer} setUserID ={setUserID} userID = {userID} setUsername1 ={setUsernameHeader} />} ></Route>
-            <Route path="/avatar" element = {<ChooseAvatar headerRef={header} footerRef ={footer} setUserPicture = {setUserPictureHeader} />} ></Route>
+            <Route path="/avatar" element = {<ChooseAvatar headerRef={header} footerRef ={footer} setUserPicture = {setUserPictureHeader} />} ></Route>           
+            <Footer footerRef = {footer}/>*/}
           </Routes>
-          <Footer footerRef = {footer}/>*/}
+          <Footer />
         </div>
       </div>
     {/*</userContext.Provider>*/}
