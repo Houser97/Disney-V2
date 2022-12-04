0bdacd4f17e4947d4eeee4015d5ae7d81d86bebb
Houser97 <arturo.riverar97@gmail.com>
2022-12-04 14:44:22 -0600

    Set up component

---
+++ client/src/components/MovieSeriesCarousel.js
@@ -0,0 +1,35 @@
+import '../styles/MovieSeriesCarousel.css';
+import MovieCard from './MovieSerieCard';
+import Slider from 'react-slick';
+
+const MovieSeriesCarousel = ({movies, title}) => {
+
+    const settings = {
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        autoplay: false,
+    }
+            
+    return(
+        <div className='container-movies'>
+            <div className='title-section'>{title}</div>
+            <div className='images-section-movies' id='images-section-movies'>
+                <Slider {...settings} className = "slider-section-movies">
+                {
+                    movies.map(function iterateMovies(movie, i){
+                        return(
+                            <MovieCard key={`${i}-movie-carousel-section`} movie = {movie} />
+                        )
+                    })
+                }
+                </Slider>
+            </div>
+            
+        </div>
+    )
+}
+
+export default MovieSeriesCarousel;
