08241bb279fba88c7054dc6ea4f59bbe45677891
Houser97 <arturo.riverar97@gmail.com>
2022-12-04 17:45:14 -0600

    Set up component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,4 @@
-import '../styles/Home.css';
+/*import '../styles/Home.css';*/
 import Carousel from './Carousel';
 import MainCategories from './MainCategories'
 /*import Movies from './moviesSection';*/
