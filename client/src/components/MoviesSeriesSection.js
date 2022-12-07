b1f61bab0af8776611ae41d7e72b317e316a3d6b
Houser97 <arturo.riverar97@gmail.com>
2022-12-06 20:11:18 -0600

    Remove unused UseState and pass Movies/Series objects as props
    The Movies/Series objects are passed as props to the MovieCarousel
    component.

--- client/src/components/MoviesSeriesSection.js
+++ client/src/components/MoviesSeriesSection.js
@@ -2,58 +2,18 @@ import '../styles/MoviesSeriesSection.css'
 import { AnimatedMovies, AnimatedSeries, Recommended, MarvelMovies } from '../assets/constants'
 
 const MoviesSeriesSection = () => {
-
-    const [recommendedMovies] = useState([{image: Daredevil},
-                                          {image: Luca},
-                                          {image: Malcolm},
-                                          {image: ObiWan},
-                                          {image: Rio},
-                                          {image: Luke},
-                                          {image: Chip},
-                                          {image: Moon}])
-
     const titleRecommendedSection = "Recommended for you"
-
-    const [marvelMovies] = useState([{image: Punisher},
-        {image: Jessica},
-        {image: IronFist},
-        {image: Loki},
-        {image: Moon},
-        {image: Luke},
-        {image: Daredevil},
-        {image: Defenders}])
-
     const titleMarvelSection = "Marvel Series and Specials"
-
-    const [animatedMovies] = useState([{image: Increibles},
-        {image: Robots},
-        {image: Rio},
-        {image: Toystory},
-        {image: Dino},
-        {image: WallE},
-        {image: Chip},
-        {image: Raya}])
-
     const titleAnimatedMoviesSection = "Animated Movies"
-
-    const [animatedSeries] = useState([{image: Mickey},
-        {image: Bluey},
-        {image: Mira},
-        {image: Bymax},
-        {image: StarWarsAn},
-        {image: Kim},
-        {image: Dalmatas},
-        {image: Kusko}])
-
     const titleAnimatedSeriesSection = "Animated Series"
 
 
     return(
         <div className='movies-series-section'>
-            <MovieCarousel key={"movie-carousel-1"} movies={recommendedMovies} title = {titleRecommendedSection} />
-            <MovieCarousel key={"movie-carousel-2"} movies={marvelMovies} title = {titleMarvelSection} />
-            <MovieCarousel key={"movie-carousel-3"} movies={animatedMovies} title = {titleAnimatedMoviesSection} />
-            <MovieCarousel key={"movie-carousel-4"} movies={animatedSeries} title = {titleAnimatedSeriesSection} />
+            <MovieCarousel key={"movie-carousel-1"} movies={Recommended} title = {titleRecommendedSection} />
+            <MovieCarousel key={"movie-carousel-2"} movies={MarvelMovies} title = {titleMarvelSection} />
+            <MovieCarousel key={"movie-carousel-3"} movies={AnimatedMovies} title = {titleAnimatedMoviesSection} />
+            <MovieCarousel key={"movie-carousel-4"} movies={AnimatedSeries} title = {titleAnimatedSeriesSection} />
         </div>
     )
 }
