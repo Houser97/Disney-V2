755897562f66c35c9a12ea51a1c3397c9f45383d
Houser97 <arturo.riverar97@gmail.com>
2022-12-09 21:10:46 -0600

    Create HeaderRef and pass it as prop to Header and MoviesAndSeries
    This reference will be used to target the Header component from the
    App component, which will allow to pass it as a prop to the
    MoviesAndSeries component to alter the opacity of the newly created div
    in the Header.

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,24 @@ import HeaderSD from './components/HeaderSD';
 import Home from './components/Home';
 import Search from './components/Search';
 import MoviesAndSeries from './components/MoviesAndSeries';
+import { useRef } from 'react';
 
 function App() {
+
+  const HeaderRef = useRef(null)
+
   return (
     <BrowserRouter basename='/'>
     {/*<userContext.Provider value={valueProvider}>*/}
       <div className="App">
         <div className='full-height'>
-          <Header userID = {false}/>
+          <Header userID = {false} HeaderRef={HeaderRef}/>
           <HeaderSD />
           <Routes >
             <Route path='/' element = {<Home />}/>
             <Route path='/search' element = {<Search />}/>
-            <Route path='/movies' element = {<MoviesAndSeries key={"movieSectionRender"} titleSection = {"Movies"}  /*headerRefPlaceholder = {header}*/ />} ></Route>
-            <Route path='/series' element = {<MoviesAndSeries key={"serieSectionRender"} titleSection = {"Series"} /*headerRefPlaceholder = {header}*/ />} ></Route>
+            <Route path='/movies' element = {<MoviesAndSeries key={"movieSectionRender"} titleSection = {"Movies"}  HeaderRef={HeaderRef} />} ></Route>
+            <Route path='/series' element = {<MoviesAndSeries key={"serieSectionRender"} titleSection = {"Series"} HeaderRef={HeaderRef} />} ></Route>
             {/*<Route path='/watchlist' element = {<Watchlist userID={userID} />} ></Route>
             <Route path='/originals' element = {<Originals key={"originals-component"} headerRefPlaceholder = {header}/>}/>
             <Route path='/login' element = {<LogInSection formToOpen={"1"} key={"logInSection"} headerRef={header} footerRef = {footer} setUserID ={setUserID} userID = {userID} setUsername1 ={setUsernameHeader} />} ></Route>
