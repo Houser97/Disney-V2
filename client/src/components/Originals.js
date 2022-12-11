13748567edf3a382f6d80c6d8ce5fdab1babd895
Houser97 <arturo.riverar97@gmail.com>
2022-12-11 14:29:48 -0600

    Remove logic to scale placeholder container

--- client/src/components/Originals.js
+++ client/src/components/Originals.js
@@ -6,14 +6,11 @@ import { movies } from '../assets/constants';
 const Originals = ({}) => {
     const [originals, setOriginals] = useState(movies)
     const originalsContainer = useRef(null);
-    const originalsPlaceholder = useRef(null);
 
     const changeTitle = () => {
         if(document.documentElement.scrollTop !== 0){
-            originalsPlaceholder.current.style.transform = 'scaleY(0.5)'
             originalsContainer.current.style.transform = 'scale(0.5)'
         } else {
-            originalsPlaceholder.current.style.transform = 'scaleY(1)'
             originalsContainer.current.style.transform = 'scale(1)'
         }
     }
@@ -33,7 +30,7 @@ const Originals = ({}) => {
 
     return(
         <div className='originals-section'>
-            <div ref={originalsPlaceholder} className='originals-placeholder'></div>
+            <div className='originals-placeholder'></div>
             {/*El elemento anterior sirve para como fondo para que las tarjetas se recorten
             alcanzando el título de ORIGINALS, ya que ORIGINALS se encoge tanto vertical como horizontalmente,
             lo cual requiere que haya un contenedor abajo de width 100% que pueda hacerla de recorte. */}
