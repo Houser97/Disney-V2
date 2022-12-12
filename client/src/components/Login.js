cb7214ca758e88d101d586a03909ed164fec1574
Houser97 <arturo.riverar97@gmail.com>
2022-12-11 18:21:50 -0600

    Set up Login component

---
+++ client/src/components/Login.js
@@ -0,0 +1,17 @@
+import '../styles/Login.css';
+import Logo from "../images/disney-logo.png";
+import FormLogin from './FormLogin';
+import FormSignUp from './formSignUp';
+
+const Login = ({formToOpen, setUsername1, setUserID, userID}) => {
+    return(
+        <div className='log-in-section'>
+            <div className='image-log-in'>
+                <img src={Logo} alt = "disney-logo" className='disney-logo-login'></img>
+            </div>
+            {formToOpen === "1" ? <FormLogin setUserID ={setUserID} userID = {userID} /> : <FormSignUp setUsername1 = {setUsername1} />}
+        </div>
+    )
+}
+
+export default Login;
