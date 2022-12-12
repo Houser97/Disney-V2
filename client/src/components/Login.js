f863454335749793716a81bf53971ad1c975d12e
Houser97 <arturo.riverar97@gmail.com>
2022-12-12 13:21:38 -0600

    Import and set FormSignup component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,7 @@
 import '../styles/Login.css';
 import logo from '../assets/images/disney-logo.png';
 import FormLogin from './FormLogin';
-/*import FormSignUp from './formSignUp';*/
+import FormSignup from './FormSignup';
 
 const Login = ({formToOpen, setUsername1, setUserID, userID}) => {
     return(
@@ -9,7 +9,7 @@ const Login = ({formToOpen, setUsername1, setUserID, userID}) => {
             <div className='image-log-in'>
                 <img src={logo} alt = "disney-logo" className='disney-logo-login'></img>
             </div>
-            {formToOpen === "1" ? <FormLogin setUserID ={setUserID} userID = {userID} /> : /*<FormSignUp setUsername1 = {setUsername1} />*/ 'Upcoming'}
+            {formToOpen === "1" ? <FormLogin setUserID ={setUserID} userID = {userID} /> : <FormSignup setUsername1 = {setUsername1} />}
         </div>
     )
 }
