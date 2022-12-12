7f90fc6e97040a92bb6dd4876343da43a3d8b32b
Houser97 <arturo.riverar97@gmail.com>
2022-12-12 17:04:43 -0600

    Fix identation and move username to .signOut-user container

--- client/src/components/UserMenu.js
+++ client/src/components/UserMenu.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import '../styles/UserMenu.css'
+import DarthVader from '../assets/images/Avatars/DarthVaderAvatar.png'
 
 const UserMenu = () => {
   return (
     <div className='profile-user'>
-        <div className='username-header'>Username</div>
-            <div className='profile-picture-header'>
-                <img /*src={userPicture}*/ alt ="headerpicture" className='header-picture-profile' ></img>
-            </div>
+        <div className='profile-picture-header'>
+            <img src={DarthVader} alt ="headerpicture" className='header-picture-profile' ></img>
+        </div>
         <div className='signOut-user'>
+            <div className='username-header'>Username</div>
             <div className='button-log-out'>
                 Log Out
             </div>
