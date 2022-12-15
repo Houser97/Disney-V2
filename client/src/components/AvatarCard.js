bbc54e78a3e8a61a09ffce5898f0023f1ed7a2ed
Houser97 <arturo.riverar97@gmail.com>
2022-12-14 21:35:08 -0600

    Remove unused logic to create user with firebase

--- client/src/components/AvatarCard.js
+++ client/src/components/AvatarCard.js
@@ -1,22 +1,23 @@
 import '../styles/AvatarCard.css';
 import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
-import { userContext } from '../App';
+/*import { userContext } from '../App';*/
 
 const AvatarCard = ({avatarImage, setUserPicture}) => {
     const navigate = useNavigate()
 
-    const setShouldRegisterNewUser = useContext(userContext)[3];
+    /*const setShouldRegisterNewUser = useContext(userContext)[3];
+
 
     const createUserOnClick = (e) => {
-        /*console.log(e.target.childNodes[0].src);*/
+        /*console.log(e.target.childNodes[0].src);
         setUserPicture(e.target.childNodes[0].src);
         setShouldRegisterNewUser("yes");
         navigate("/")
-    }
+    }*/
 
     return(
-        <div className='avatarCard' onClick={createUserOnClick}>
+        <div className='avatarCard' /*onClick={createUserOnClick}*/>
             <img src = {avatarImage} alt = "avatar" className='avatar-image'></img>
         </div>
     )
