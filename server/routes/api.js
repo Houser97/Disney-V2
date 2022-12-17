5596545ebbc65deea77deeb8073d1834528da87d
Houser97 <arturo.riverar97@gmail.com>
2022-12-16 22:45:43 -0600

    Define signup route and set create_user controller

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,3 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const apiController = require('../controllers/apiControllers');
+
+// API para crear usuario.
+router.get('/signup', apiController.create_user);
