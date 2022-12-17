dd939352ec2d9d29ff61811721a2d994bf29dea0
Houser97 <arturo.riverar97@gmail.com>
2022-12-16 22:16:24 -0600

    Validate and sanitize email and password entered by users

--- server/controllers/apiControllers.js
+++ server/controllers/apiControllers.js
@@ -1,2 +1,15 @@
 const User = require('../models/user');
-const {body, validationResult} = require('express-validator')
+const {body, validationResult} = require('express-validator');
+
+exports.create_user = [
+    body('email', 'Email must be a valid address').isEmail()
+        .trim()
+        .escape()
+        .normalizeEmail(),
+    body('pwd', 'Password must not be empty')
+        .isLength({min:8}).withMessage('Password must contain at least 8 characters')
+        .matches('[0-9]').withMessage('Password must contain at least 1 number')
+        .matches('[A-Z]').withMessage('Password must contain at least 1 upper letter')
+        .trim()
+        .escape()
+];
