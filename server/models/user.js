80e0a64918176bb5b0001b52ba678a8e59ddc958
Houser97 <arturo.riverar97@gmail.com>
2022-12-19 13:14:13 -0600

    Change avatar and watchlist data type to numeric

--- server/models/user.js
+++ server/models/user.js
@@ -5,8 +5,8 @@ const UserSchema = new Schema({
     username: {type: String, required: true},
     password: {type: String, required: true},
     email: {type: String, required: true},
-    avatar: {type: String, required: true},
-    watchlist: [{type: String}],
+    avatar: {type: Number, required: true},
+    watchlist: [{type: Number}],
 })
 
 module.exports = mongoose.model('User', UserSchema);
