201d156529ffb3800c8565f50ba1f6089cd36fec
Houser97 <arturo.riverar97@gmail.com>
2022-12-19 13:15:04 -0600

    Change get method to post for signup route

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const apiController = require('../controllers/apiControllers');
 
 // API para crear usuario.
-router.get('/signup', apiController.create_user);
+router.post('/signup', apiController.create_user);
 
 router.get('/login', apiController.login);
 
