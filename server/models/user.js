255daaf144d010305669475bb0349adfe439d037
Houser97 <arturo.riverar97@gmail.com>
2022-12-20 17:34:16 -0600

    Change avatar data type to String

--- server/models/user.js
+++ server/models/user.js
@@ -5,7 +5,7 @@ const UserSchema = new Schema({
     username: {type: String, required: true},
     password: {type: String, required: true},
     email: {type: String, required: true},
-    avatar: {type: Number, required: true},
+    avatar: {type: String, required: true},
     watchlist: [{type: Number}],
 })
 
