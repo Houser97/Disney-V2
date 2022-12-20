fb93aa68c07f29eec9486f223893dc776da31cb2
Houser97 <arturo.riverar97@gmail.com>
2022-12-20 09:21:31 -0600

    Change props with avatar object

--- client/src/components/AvatarCard.js
+++ client/src/components/AvatarCard.js
@@ -3,22 +3,20 @@ import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 /*import { userContext } from '../App';*/
 
-const AvatarCard = ({avatarImage, setUserPicture}) => {
+const AvatarCard = ({avatar}) => {
     const navigate = useNavigate()
 
-    /*const setShouldRegisterNewUser = useContext(userContext)[3];
 
-
-    const createUserOnClick = (e) => {
+    const UpdateAvatar = (e) => {
         /*console.log(e.target.childNodes[0].src);
         setUserPicture(e.target.childNodes[0].src);
-        setShouldRegisterNewUser("yes");
+        setShouldRegisterNewUser("yes");*/
         navigate("/")
-    }*/
+    }
 
     return(
-        <div className='avatarCard' /*onClick={createUserOnClick}*/>
-            <img src = {avatarImage} alt = "avatar" className='avatar-image'></img>
+        <div className='avatarCard' onClick={UpdateAvatar}>
+            <img src = {avatar.image} alt = "avatar" className='avatar-image'></img>
         </div>
     )
 }
