830f43842b2ac468ed8bf92105f7ba944e9f706f
Houser97 <arturo.riverar97@gmail.com>
2022-12-21 12:31:21 -0600

    Import isUserLogged, set watchlist state and useEffect to update it

--- client/src/components/MoviesSeriesSection.js
+++ client/src/components/MoviesSeriesSection.js
@@ -1,6 +1,8 @@
 import '../styles/MoviesSeriesSection.css'
 import { AnimatedMovies, AnimatedSeries, Recommended, MarvelMovies } from '../assets/constants'
 import MovieSeriesCarousel from './MovieSeriesCarousel'
+import { useContext, useEffect, useState } from 'react'
+import { userContext } from '../App'
 
 
 const MoviesSeriesSection = () => {
@@ -8,6 +10,17 @@ const MoviesSeriesSection = () => {
     const titleMarvelSection = "Marvel Series and Specials"
     const titleAnimatedMoviesSection = "Animated Movies"
     const titleAnimatedSeriesSection = "Animated Series"
+
+    const isUser = useContext(userContext)[0]
+
+    const [watchlist, setWatchlist] = useState(null)
+
+    useEffect(() => {
+      if(isUser){
+        setWatchlist(isUser.watchlist)
+      }
+    }, [isUser])
+    
 
 
     return(
