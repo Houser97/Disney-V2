3999c5a51001c5b0517ab9f464c9426171f70580
Houser97 <arturo.riverar97@gmail.com>
2022-12-20 19:58:50 -0600

    Get avatar image and username from user object

--- client/src/components/UserMenuSD.js
+++ client/src/components/UserMenuSD.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/UserMenuSD.css'
 import DarthVader from '../assets/images/Avatars/DarthVaderAvatar.png'
+import { AvatarImages } from '../assets/constants'
 
-const UserMenuSD = () => {
+const UserMenuSD = ({user}) => {
+
+  const [avatar, setAvatar] = useState(null)
+  const [username, setUsername] = useState(null)
+
+  useEffect(() => {
+    /*const user = JSON.parse(sessionStorage.getItem('user'));*/
+    const avatarImage = AvatarImages.filter(avatarObject => avatarObject.ref === user.avatar)
+    setAvatar(avatarImage[0].image);
+    setUsername(user.username)
+    console.log(avatarImage)
+  }, [user])
+
   return (
     <div className='profile-user-SD'>
         <div className='profile-picture-header'>
-            <img src={DarthVader} alt ="headerpicture" className='header-picture-profile' ></img>
+            <img src={avatar} alt ="headerpicture" className='header-picture-profile' ></img>
         </div>
         <div className='signOut-user-SD'>
-            <div className='username-header'>Username</div>
+            <div className='username-header'>{username}</div>
             <div className='button-log-out'>
                 Log Out
             </div>
