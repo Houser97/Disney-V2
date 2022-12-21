118f25b0eff08d54faefa1af05d63ab56309d3c9
Houser97 <arturo.riverar97@gmail.com>
2022-12-21 11:34:31 -0600

    Set logout route

--- server/routes/api.js
+++ server/routes/api.js
@@ -10,6 +10,8 @@ router.post('/signup', apiController.create_user);
 
 router.get('/login', apiController.login);
 
+router.get('/logout', apiController.logout);
+
 router.post('/check_email', apiController.check_email);
 
 router.post('/update_avatar', apiController.update_avatar);
