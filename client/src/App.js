9e82a597314da47f9e73619a201016b0275ab1e8
Houser97 <arturo.riverar97@gmail.com>
2022-12-20 18:53:21 -0600

    Set isUserLogged state and userContext
    The userContext will be used to prevent prop drilling to pass the
    setIsUserLogged function to components that will check whether the
    backend says there is a user logged or not.

--- client/src/App.js
+++ client/src/App.js
@@ -10,31 +10,36 @@ import Watchlist from './components/Watchlist';
 import Originals from './components/Originals';
 import Login from './components/Login';
 import Avatar from './components/Avatar';
+import { createContext, useEffect, useState } from 'react';
+
+export const userContext = createContext()
+
+const [isUserLogged, setIsUserLogged] = useState(null)
 
 function App() {
   return (
     <BrowserRouter basename='/'>
-    {/*<userContext.Provider value={valueProvider}>*/}
-      <div className="App">
-        <div className='full-height'>
-          <Header userID = {false}/>
-          <HeaderSD />
-          <Routes >
-            <Route path='/' element = {<Home />}/>
-            <Route path='/search' element = {<Search />}/>
-            <Route path='/movies' element = {<MoviesAndSeries key={"movieSectionRender"} titleSection = {"Movies"} />} ></Route>
-            <Route path='/series' element = {<MoviesAndSeries key={"serieSectionRender"} titleSection = {"Series"} />} ></Route>
-            <Route path='/watchlist' element = {<Watchlist userID={null} />} ></Route>
-            <Route path='/originals' element = {<Originals key={"originals-component"}/>}/>
-            <Route path='/login' element = {<Login formToOpen={"1"} key={"logInSection"} /*headerRef={header} footerRef = {footer} setUserID ={setUserID} userID = {userID} setUsername1 ={setUsernameHeader}*/ />} ></Route>
-            <Route path='/signup' element = {<Login formToOpen={"2"} key={"signUpSection"}  /*setUsername1 ={setUsernameHeader}*/ />} ></Route>
-            <Route path="/avatar" element = {<Avatar /*setUserPicture = {setUserPictureHeader}*/ />} ></Route>           
-          </Routes>
-          <Footer />
+      <userContext.Provider value={valueProvider}>
+        <div className="App">
+          <div className='full-height'>
+            <Header userID = {false}/>
+            <HeaderSD />
+            <Routes >
+              <Route path='/' element = {<Home />}/>
+              <Route path='/search' element = {<Search />}/>
+              <Route path='/movies' element = {<MoviesAndSeries key={"movieSectionRender"} titleSection = {"Movies"} />} ></Route>
+              <Route path='/series' element = {<MoviesAndSeries key={"serieSectionRender"} titleSection = {"Series"} />} ></Route>
+              <Route path='/watchlist' element = {<Watchlist userID={null} />} ></Route>
+              <Route path='/originals' element = {<Originals key={"originals-component"}/>}/>
+              <Route path='/login' element = {<Login formToOpen={"1"} key={"logInSection"} /*headerRef={header} footerRef = {footer} setUserID ={setUserID} userID = {userID} setUsername1 ={setUsernameHeader}*/ />} ></Route>
+              <Route path='/signup' element = {<Login formToOpen={"2"} key={"signUpSection"}  /*setUsername1 ={setUsernameHeader}*/ />} ></Route>
+              <Route path="/avatar" element = {<Avatar /*setUserPicture = {setUserPictureHeader}*/ />} ></Route>           
+            </Routes>
+            <Footer />
+          </div>
         </div>
-      </div>
-    {/*</userContext.Provider>*/}
-  </BrowserRouter>
+      </userContext.Provider>
+    </BrowserRouter>
   );
 }
 
