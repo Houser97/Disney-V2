a20370acdc6ddcbd0913b4128654a5faeaf11d7d
Houser97 <arturo.riverar97@gmail.com>
2022-12-22 08:51:36 -0600

    Remove comments and unnecessary lines of code

--- client/src/components/MoviesAndSeries.js
+++ client/src/components/MoviesAndSeries.js
@@ -14,47 +14,21 @@ const MoviesAndSeries = ({titleSection}) => {
     const [moviesOrSeries, setMoviesOrSeries] = useState(MoviesOrSeries);
     const [filter, setFilter] = useState("ALL MOVIES A-Z");
 
-    const containerToFix = useRef(null);
-    /*
-    const fixTitleSection = () => {
-        if(document.documentElement.scrollTop !== 0){
-            containerToFix.current.style.position = "fixed";
-        } else {
-            containerToFix.current.style.position = "relative";
-            header.style.backgroundColor = "rgb(26, 29, 41)" 
-        }
-    }*/
-
     useEffect(() => {
         if(filter === "ANIMATED"){
             setMoviesOrSeries(MoviesOrSeries.filter(movie => movie.isAnimated));
         } else if (filter === "KIDS"){
             setMoviesOrSeries(MoviesOrSeries.filter(movie => movie.isForKids))
-        } else if (filter === "ALL MOVIES A-Z") {
+        } else {
             setMoviesOrSeries(MoviesOrSeries)
         }
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [filter])
 
-    /*useEffect(() => {
-        window.addEventListener("scroll", fixTitleSection);
-        header.style.backgroundColor = "rgb(26, 29, 41)" 
-
-        return () => {
-            window.removeEventListener("scroll", fixTitleSection);
-            if(document.documentElement.scrollTop !== 0){
-                header.style.backgroundColor = "rgb(14, 16, 26)";
-            }
-            else {header.style.backgroundColor = "transparent";}
-        }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])*/
-
-
     return(
         <div className='moviesAndSeries-section'>
             <div className='title-section-placeholder'>
-                <div /*ref={containerToFix}*/ className='title-section-container'>
+                <div className='title-section-container'>
                     <div className='title-section-movies-series'>{titleSection}</div>
                     <DropdownMenu setFilter={setFilter} />
                 </div>
