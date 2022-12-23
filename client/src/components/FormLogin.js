d16cbc0999a602a41f2b28a8f4b7aafb93755e73
Houser97 <arturo.riverar97@gmail.com>
2022-12-22 18:33:44 -0600

    Add comment and remove unused logic

--- client/src/components/FormLogin.js
+++ client/src/components/FormLogin.js
@@ -5,16 +5,14 @@ import { useNavigate } from 'react-router-dom';
 import useWindowSize from '../assets/hooks/windowSize.js';
 import { userContext } from '../App';
 
-const FormLogIn = ({setUserID, userID}) => {
+const FormLogIn = () => {
 
     let navigate = useNavigate()
     const windowSize = useWindowSize()
 
     const [isMobile, setIsMobile] = useState(windowSize.width <= 470)
 
-    const emailSection = useRef(null);
-    const pwdSection = useRef(null);
-    const containerForm = useRef(null);
+    const containerForm = useRef(null); //Se usa para mover los formularios
 
     const [email, setEmail] = useState(null);
     const [password, setPassword] = useState(null);
@@ -47,21 +45,7 @@ const FormLogIn = ({setUserID, userID}) => {
             })
         }
     }, [password])
-/*
-    const [userData, setUserData] = useState([]);
 
-    useEffect(() => {
-        if(userData.length === 2){
-            const email = userData[0];
-            console.log(email);
-            const pwd = userData[1];
-            console.log(pwd);
-            navigate("/")
-        }
-        //console.log(userData)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [userData])
-*/
     const handleSubmit = (e) => {
         e.preventDefault();
         const TranslateX = isMobile ? 280 : 370;
@@ -79,7 +63,7 @@ const FormLogIn = ({setUserID, userID}) => {
         <div className='form-log-in-sign-up'>
             <div className='form-data-carousel'>
                 <div ref={containerForm} className='form-container'>
-                    <form ref={emailSection} className='email-section' onSubmit={handleSubmit}>
+                    <form  className='email-section' onSubmit={handleSubmit}>
                         <div className='input-label-login'>
                             <label htmlFor='login'>Log in with your email</label>
                             <input id='login' className='input-login' type="email" required></input>
@@ -89,7 +73,7 @@ const FormLogIn = ({setUserID, userID}) => {
                         </div>
                     </form>
 
-                    <form ref={pwdSection} className='pdw-section' onSubmit={handleLastSubmit}>
+                    <form className='pdw-section' onSubmit={handleLastSubmit}>
                         <div className='input-label-login'>
                             <label htmlFor='login'>Enter your password</label>
                             <input id='pwd' className='input-login' type="password" required></input>
