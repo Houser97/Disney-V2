00297e6e22851f3dd3b1d36a699e2c2f4e388903
Houser97 <arturo.riverar97@gmail.com>
2022-12-22 18:51:31 -0600

    Remove unused props

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,13 +3,13 @@ import logo from '../assets/images/disney-logo.png';
 import FormLogin from './FormLogin';
 import FormSignup from './FormSignup';
 
-const Login = ({formToOpen, setUsername1, setUserID, userID}) => {
+const Login = ({formToOpen}) => {
     return(
         <div className='log-in-section'>
             <a className='image-log-in' href='/'>
                 <img src={logo} alt = "disney-logo" className='disney-logo-login'></img>
             </a>
-            {formToOpen === "1" ? <FormLogin setUserID ={setUserID} userID = {userID} /> : <FormSignup setUsername1 = {setUsername1} />}
+            {formToOpen === "1" ? <FormLogin /> : <FormSignup />}
         </div>
     )
 }
