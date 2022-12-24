99ab7962c1ee1a7b5b85671031e2b2f84a673324
Houser97 <arturo.riverar97@gmail.com>
2022-12-24 12:06:17 -0600

    Use isUserLogged for conditional rendering

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,14 +2,22 @@ import '../styles/Login.css';
 import logo from '../assets/images/disney-logo.png';
 import FormLogin from './FormLogin';
 import FormSignup from './FormSignup';
+import { userContext } from '../App';
+import { useContext } from 'react';
 
 const Login = ({formToOpen}) => {
+
+    const isUser = useContext(userContext).isUserLogged;
+
     return(
         <div className='log-in-section'>
             <a className='image-log-in' href='/'>
                 <img src={logo} alt = "disney-logo" className='disney-logo-login'></img>
             </a>
-            {formToOpen === "1" ? <FormLogin /> : <FormSignup />}
+            {isUser ? (<div className='user-already-logged'>You are already logged in</div>):(
+                formToOpen === "1" ? <FormLogin /> : <FormSignup />
+            )}
+            
         </div>
     )
 }
