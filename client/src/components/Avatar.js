861f2d214781c2e4a28e7a54b6fd424b4ee0a054
Houser97 <arturo.riverar97@gmail.com>
2022-12-24 11:50:28 -0600

    User isUserLoggedIn for conditional rendering

--- client/src/components/Avatar.js
+++ client/src/components/Avatar.js
@@ -1,19 +1,27 @@
 import '../styles/Avatar.css';
 import AvatarCard from './AvatarCard';
 import { AvatarImages } from '../assets/constants';
+import { userContext } from '../App';
+import { useContext } from 'react';
 
 const Avatar = () => {
 
+    const isUser = useContext(userContext).isUserLogged;
+
     return(
         <div className='choose-avatar-section'>
-            <div className='instruction-avatar'>Choose Avatar</div>
-            <div className='avatars-grid'>
-                {AvatarImages.map(function iterateAvatars(avatar, i){
-                    return(
-                        <AvatarCard key={`${i}-avatar-image`} avatar = {avatar} />
-                    )
-                })}
+            {isUser ? (
+                <div>
+                    <div className='instruction-avatar'>Choose Avatar</div>
+                    <div className='avatars-grid'>
+                        {AvatarImages.map(function iterateAvatars(avatar, i){
+                            return(
+                                <AvatarCard key={`${i}-avatar-image`} avatar = {avatar} />
+                            )
+                        })}
+                    </div>
             </div>
+            ):(<div className='avatar-no-user'>Log in in order to update avatar</div>)}
         </div>
     )
 }
