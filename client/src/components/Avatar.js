21e523ab12fccf940141dfb80550077e035b6ef9
Arturo Rivera <arturo.riverar97@gmail.com>
2023-04-22 17:21:28 -0600

    Set up Loading component

--- client/src/components/Avatar.js
+++ client/src/components/Avatar.js
@@ -2,11 +2,26 @@ import '../styles/Avatar.css';
 import AvatarCard from './AvatarCard';
 import { AvatarImages } from '../assets/constants';
 import { userContext } from '../App';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 
 const Avatar = () => {
 
     const isUser = useContext(userContext).isUserLogged;
+    const setIsUserLogged = useContext(userContext).setIsUserLogged;
+    const API = useContext(userContext).API;
+
+    //El useEffect se necesita para usuarios que acaban de hacer signup.
+    useEffect(() => {
+        fetch(`${API}/api/check_if_user_is_logged`, {
+            method: 'GET',
+            credentials: 'include'
+        })
+        .then(response => response.json())
+        .then(data => {
+          sessionStorage.setItem('user', JSON.stringify(data))
+          setIsUserLogged(data)
+        })
+      }, [])    
 
     return(
         <div className='choose-avatar-section'>
@@ -14,9 +29,9 @@ const Avatar = () => {
                 <div>
                     <div className='instruction-avatar'>Choose Avatar</div>
                     <div className='avatars-grid'>
-                        {AvatarImages.map(function iterateAvatars(avatar){
+                        {AvatarImages.map(function iterateAvatars(avatar, i){
                             return(
-                                <AvatarCard key={`${avatar.ref}-avatar-image`} avatar = {avatar} />
+                                <AvatarCard key={`${i}-avatar-image`} avatar = {avatar} />
                             )
                         })}
                     </div>
