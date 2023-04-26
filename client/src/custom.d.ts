942d49baba649a0bed9450193ab32ac7656a9964
Arturo Rivera <arturo.riverar97@gmail.com>
2023-04-25 20:30:37 -0600

    Set custom ts file to declare modules to import png, JPG and mp4 files

---
+++ client/src/custom.d.ts
@@ -0,0 +1,14 @@
+declare module '*.png' {
+    const value: any;
+    export = value;
+}
+
+declare module '*.JPG' {
+    const value: any;
+    export = value;
+}
+
+declare module '*.mp4' {
+    const value: any;
+    export = value;
+}
